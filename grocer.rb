a3aee357f31123870151119713c6d8ec90c3be1a
Caroline S-A <caroline.seilern@hotmail.com>
2019-09-30 07:50:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,9 +46,9 @@ def apply_clearance(cart)
     
   if cart[item]
     
-    if cart[item][:clearance]=true
+    if cart[item][:clearance]
       
-      cart[item][:price] = (cart[item][:price]* 0.8).round(2)
+      cart[item][:price] = cart[item][:price]* 0.8
     end
   end
   end
